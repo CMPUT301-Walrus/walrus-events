5b1d38fa884b702e11c4424432b726f0fa4fc834
LuMilne <lemilne@ualberta.ca>
2026-03-03 16:16:44 -0700

    added unfinished Lottery class

---
+++ WalrusEvents/app/src/main/java/com/example/walrusevents/Lottery.java
@@ -0,0 +1,15 @@
+package com.example.walrusevents;
+
+/*
+Functionality:
+ + Randomly* select X entrants on an event's waitlist. These are the winners. All others are losers.
+ ? Update the Event with the winning applicants(Maybe not. If the only way to lock-in is through the notifs then maybe it can be handled when they accept.)
+ + Alert notifications system to send winners/losers notifications that they won/lost
+What we need:
+ + The event we are drawing for
+ + The list of applicants
+ + The maximum number of entrants
+ + Contact with the notification system, and relevant information to pass so that it may
+ */
+public class Lottery {
+}
