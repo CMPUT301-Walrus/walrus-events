1303e959062bcb2f94949fecdf09612a9af5b516
Atonize <aamalia@ualberta.ca>
2026-03-26 21:49:17 -0600

    feat: comment model

--- WalrusEvents/app/src/main/java/com/example/walrusevents/model/Comment.java
+++ WalrusEvents/app/src/main/java/com/example/walrusevents/model/Comment.java
@@ -3,4 +3,46 @@ package com.example.walrusevents.model;
 public class Comment {
     private String entrantId;
     private String body;
+    private int likes;
+    private int dislikes;
+
+    public String getEntrantId() {
+        return entrantId;
+    }
+
+    public void setEntrantId(String entrantId) {
+        this.entrantId = entrantId;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public void addLike() {
+        likes += 1;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(int dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    public void addDislike() {
+        dislikes += 1;
+    }
 }
