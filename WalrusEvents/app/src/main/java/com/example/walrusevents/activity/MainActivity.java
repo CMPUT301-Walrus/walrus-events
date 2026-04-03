0e136828ff9f13c7e61311924033061c12c5cb59
User <neilakhu87@gmail.com>
2026-04-03 14:17:32 -0600

    wip: calendar fragment (for availability filter)

--- WalrusEvents/app/src/main/java/com/example/walrusevents/activity/MainActivity.java
+++ WalrusEvents/app/src/main/java/com/example/walrusevents/activity/MainActivity.java
@@ -127,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
                 //GO TO SCHEDULE FRAGMENT
                 //TODO: setup fragment and get the ModalDateRangePicker
 
+
             }else{
                 //revert?
             }
