ed1168596b6e5b458454eaf8327cc7e0d553cf01
User <neilakhu87@gmail.com>
2026-04-06 05:41:56 -0600

    feature: invite entrant by personal information for private event.

---
+++ WalrusEvents/app/src/main/java/com/example/walrusevents/util/SearchPrivateEntrantsAdapter.java
@@ -0,0 +1,28 @@
+package com.example.walrusevents.util;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+import com.example.walrusevents.model.Entrant;
+
+import java.util.ArrayList;
+
+public class SearchPrivateEntrantsAdapter extends ArrayAdapter<Entrant> {
+    private ArrayList<Entrant> entrants;
+    private Context context;
+    private Entrant entrant;
+    /**
+     * Constructor for organizer event array adapter
+     * @param context
+     * @param entrants
+     */
+    public SearchPrivateEntrantsAdapter(@NonNull Context context, ArrayList<Entrant> entrants) {
+        super(context, 0, entrants);
+        this.entrants = entrants;
+        this.context = context;
+    }
+
+
+}
