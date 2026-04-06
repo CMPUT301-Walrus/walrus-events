1587d8117cd07dd6ba822bd5c5ecea0c48fc24ea
Nan0p <mingxin3@ualberta.ca>
2026-04-06 04:51:58 -0600

    Pre and Post Lottery(no notifications)

--- WalrusEvents/app/src/main/java/com/example/walrusevents/ui/OEventPoolView.java
+++ WalrusEvents/app/src/main/java/com/example/walrusevents/ui/OEventPoolView.java
@@ -3,18 +3,17 @@ package com.example.walrusevents.ui;
 import android.app.Activity;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.fragment.app.FragmentContainerView;
-import androidx.fragment.app.FragmentOnAttachListener;
 
 import com.example.walrusevents.R;
 
 public class OEventPoolView {
-    private Button settingsButton;
-    private Button backButton;
+    private ImageView settingsButton;
+    private ImageView backButton;
     private Button lotteryButton;
+    private Button sendNotificationsButton;
     private ImageView mapButton;
     private Button removeButton;
     private FragmentContainerView fragmentContainer;
@@ -25,6 +24,7 @@ public class OEventPoolView {
         settingsButton = context.findViewById(R.id.settings_org_button);
         backButton = context.findViewById(R.id.back_waiting_list_org);
         lotteryButton = context.findViewById(R.id.lottery_button);
+        sendNotificationsButton = context.findViewById(R.id.send_notifications_button);
         mapButton = context.findViewById(R.id.map_button);
         removeButton = context.findViewById(R.id.remove_applicant);
         fragmentContainer = context.findViewById(R.id.waiting_list_fragment);
@@ -36,15 +36,19 @@ public class OEventPoolView {
         return fragmentContainer;
     }
 
-    public Button getSettingsButton() {
+    public ImageView getSettingsButton() {
         return settingsButton;
     }
-    public Button getBackButton() {
+    public ImageView getBackButton() {
         return backButton;
     }
 
     public Button getLotteryButton() {
         return lotteryButton;
+    }
+
+    public Button getSendNotificationsButton() {
+        return sendNotificationsButton;
     }
 
     public Button getRemoveButton() {
