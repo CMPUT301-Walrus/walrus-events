4130a81c59bc185404a27b341ef4347b7736290d
ly808 <jjhunter@ualberta.ca>
2026-04-05 20:28:06 -0600

    converted back_button to ImageView in event_details.xml

--- WalrusEvents/app/src/main/java/com/example/walrusevents/ui/UEventDetailsView.java
+++ WalrusEvents/app/src/main/java/com/example/walrusevents/ui/UEventDetailsView.java
@@ -13,7 +13,7 @@ public class UEventDetailsView {
     private TextView eventName;
     private TextView description;
     private ImageView eventPoster;
-    private Button backButton;
+    private ImageView backButton;
     private Button joinButton;
     private Button seePoolButton;
     private Button viewCommentsButton;
